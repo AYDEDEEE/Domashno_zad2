74dc7063c5e5ac6d117953e48cb02ac98afc603f
IVO <IVO@AYde>
2014-09-15 05:20:54 +0300

    Adding the sorting method

--- GeekCamp/src/GITExercises/Domashno_zad2.java
+++ GeekCamp/src/GITExercises/Domashno_zad2.java
@@ -18,10 +18,66 @@ public class Domashno_zad2 {
 
 		a = o.clone();
 
-//		sorting();
+		sorting();
 //		premahvane();
 
 	}
+	
+	public static void sorting() {
+		boolean ok = false;
+		int br = 0, maxbr = 0, indfirst = 0, indsecond = 0, broqch = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			for (int k = i + 1; k < a.length; k++) {
+				if (a[i] == a[k]) {
+					broqch++;
+					if (k != i + 1) {
+						for (int l = i + 1; l < k; l++) {
+							for (int p = l + 1; p < k; p++) {
+								if (a[l] != a[p]) {
+									ok = true;
+									br++;
+								} else {
+									ok = false;
+									br = 0;
+									break;
+
+								}
+
+							}
+						}
+					} else {
+						br = 0;
+						ok = true;
+					}
+
+				}
+				if (ok == true && br >= maxbr) {
+					maxbr = br;
+					br = 0;
+					indfirst = i;
+					indsecond = k;
+				}
+
+			}
+		}
+		char[] temp = new char[a.length - 1];
+		for (int i = 0; i < indfirst; i++) {
+			temp[i] = a[i];
+		}
+		for (int i = indfirst + 1; i < indsecond; i++) {
+			temp[i] = a[i];
+		}
+		for (int i = indsecond + 1; i < temp.length; i++) {
+			temp[i] = a[i];
+		}
+		temp[temp.length - 1] = a[indsecond];
+		a = null;
+		a = temp.clone();
+		if (broqch > 1) {
+			sorting();
+		}
+		}
+	}
 
 
-}
